020335881a99a4f4ac3f2d1003971b26ef606f6e
Luciano Reyes <luchoreyes92@gmail.com>
2020-07-08 22:03:52 -0300

    creacion imprenta 203

---
+++ src/main/java/Logica/Simular.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logica;
+
+/**
+ *
+ * @author lucho
+ */
+public class Simular {
+    
+    private Double utilidad1,utilidad2;
+    
+    public Simular(Double utilidad1,Double utilidad2){
+        this.utilidad1 = utilidad1;
+        this.utilidad2 = utilidad2;
+    }
+    
+    public Object[] simular(Object[] filaAnterior){
+        
+        return null;
+    }
+}
